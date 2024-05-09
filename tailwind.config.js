4ad9bfcb3bea7a602a5886e2accb01be1976347c
ReDhaBenKortbi <ridhabenkortbi@gmail.com>
2024-05-09 10:14:15 +0100

    init files\

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          nutmeg: "#8e5b31", // hsl(14, 45%, 36%)
+          darkRaspberry: "#4e1d2d", // hsl(332, 51%, 32%)
+        },
+        neutral: {
+          white: "#ffffff", // hsl(0, 0%, 100%)
+          roseWhite: "#faf4f4", // hsl(330, 100%, 98%)
+          eggshell: "#f0e1d5", // hsl(30, 54%, 90%)
+          lightGrey: "#dcd8d4", // hsl(30, 18%, 87%)
+          wengeBrown: "#615247", // hsl(30, 10%, 34%)
+          darkCharcoal: "#2b2a29", // hsl(24, 5%, 18%)
+        },
+      },
+    },
+  },
+};
